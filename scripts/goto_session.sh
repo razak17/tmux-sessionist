340b36b63c19e9d501058fd0da1e3ce6e40c6faf
Abdul Razak <moabdulrazak17@gmail.com>
2026-08-15 23:25:19 +0000

    feat(session): add fzf-powered session switching

--- scripts/goto_session.sh
+++ scripts/goto_session.sh
@@ -3,9 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
-	# displays tmux session list
-	tmux run-shell -b "$CURRENT_DIR/list_sessions.sh"
-	# goto command prompt
-	tmux command -p session: "run '$CURRENT_DIR/switch_or_loop.sh \"%1\"'"
+  tmux run-shell -b "$CURRENT_DIR/list_sessions.sh | $CURRENT_DIR/fzf-tmux-pane.sh | xargs tmux switch-client -t || true"
 }
 main
